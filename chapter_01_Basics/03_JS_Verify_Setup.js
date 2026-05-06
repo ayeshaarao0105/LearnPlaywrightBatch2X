c79111f0ee19884fbb755f323040f49f0dbfa1d7
Ayesha Rao <ayeshaarao0105@gmail.com>
2026-05-06 09:19:46 -0700

    Initial commit

---
+++ chapter_01_Basics/03_JS_Verify_Setup.js
@@ -0,0 +1,3 @@
+console.log(process.arch);
+console.log("Node Version:",process.version);
+console.log("Platform Version:",process.platform);
---
+++ chapter_01_Basics/04_HotCode.js
@@ -0,0 +1,8 @@
+function add(a,b){
+    return a+b;
+}
+let result;
+for (let i=10;i<10000;i++){
+    result = add(i,i+1);
+}
+console.log("After 10000 calls:",result);
